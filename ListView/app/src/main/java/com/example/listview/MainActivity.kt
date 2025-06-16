4a154e2618b6f41b97fc92091b5f043c0b401602
zhangyu <pike123456@qq.com>
2025-06-16 16:14:36 +0800

    ListView

---
+++ ListView/app/src/main/java/com/example/listview/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.listview
+
+import android.os.Bundle
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.listview.Fruit
+import com.example.listview.FruitAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    private val fruitList = ArrayList<Fruit>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initFruits()
+        val adapter = FruitAdapter(this, R.layout.fruit_item, fruitList)
+        findViewById<ListView>(R.id.listView).adapter = adapter
+    }
+
+    private fun initFruits() {
+        repeat(2) {
+            fruitList.add(Fruit("Apple", R.drawable.apple_pic))
+            fruitList.add(Fruit("Banana", R.drawable.banana_pic))
+            fruitList.add(Fruit("Orange", R.drawable.orange_pic))
+            fruitList.add(Fruit("Watermelon", R.drawable.watermelon_pic))
+            fruitList.add(Fruit("Pear", R.drawable.pear_pic))
+            fruitList.add(Fruit("Grape", R.drawable.grape_pic))
+            fruitList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
+            fruitList.add(Fruit("Strawberry", R.drawable.strawberry_pic))
+            fruitList.add(Fruit("Cherry", R.drawable.cherry_pic))
+            fruitList.add(Fruit("Mango", R.drawable.mango_pic))
+        }
+    }
+
+}
