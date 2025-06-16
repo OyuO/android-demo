c4b3130d46640f64b7a497e81313ebd93922dae0
zhangyu <pike123456@qq.com>
2025-06-16 16:27:07 +0800

    ListView 优化渲染效率

--- ListView/app/src/main/java/com/example/listview/FruitAdapter.kt
+++ ListView/app/src/main/java/com/example/listview/FruitAdapter.kt
@@ -14,14 +14,36 @@ class FruitAdapter(activity: Activity, val resourceId: Int, data: List<Fruit>) :
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
-        val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
-        val fruitImage = view.findViewById<ImageView>(R.id.fruitImage)
-        val fruitName = view.findViewById<TextView>(R.id.fruitName)
+//        val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+//        val fruitImage = view.findViewById<ImageView>(R.id.fruitImage)
+//        val fruitName = view.findViewById<TextView>(R.id.fruitName)
+//        val fruit = getItem(position)
+//        if (fruit != null) {
+//            fruitImage.setImageResource(fruit.imageId)
+//            fruitName.text = fruit.name
+//        }
+//        return view
+
+        val view: View
+        val viewHolder: ViewHolder
+        if (convertView == null) {
+            view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+            val fruitImage = view.findViewById<ImageView>(R.id.fruitImage)
+            val fruitName = view.findViewById<TextView>(R.id.fruitName)
+            viewHolder = ViewHolder(fruitImage, fruitName)
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+
         val fruit = getItem(position)
         if (fruit != null) {
-            fruitImage.setImageResource(fruit.imageId)
-            fruitName.text = fruit.name
+            viewHolder.fruitImage.setImageResource(fruit.imageId)
+            viewHolder.fruitName.text = fruit.name
         }
         return view
     }
+
+    inner class ViewHolder(val fruitImage: ImageView, val fruitName: TextView)
 }
